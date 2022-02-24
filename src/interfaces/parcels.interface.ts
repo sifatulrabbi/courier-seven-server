ea231796c7943ac737913a324cf6c1f0b7604f1a
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-02-24 16:05:13 +0600

    feat(interfaces): add to parcel interface

--- src/interfaces/parcels.interface.ts
+++ src/interfaces/parcels.interface.ts
@@ -3,30 +3,25 @@ import { IAddress } from "./interfaces";
 import { IProductTypes } from "./interfaces";
 
 export interface IParcel {
-    _id?: string;
+    _id: string;
     invoice_id: string;
-    customer: {
-        name: string;
-        mobile: string;
-        address: IAddress;
-    };
-    product: {
-        price: number;
-        weight: number;
-        type: IProductTypes;
-    };
     shop: {
         shop_id: string;
         name: string;
+        email?: string;
+        mobile: string;
         address: IAddress;
     };
+    customer: {
+        name: string;
+        mobile: string;
+        email?: string;
+        address: IAddress;
+    };
+    product: { price: number; weight: number; type: IProductTypes };
     collection_amount: number;
     notes?: string;
     created_at: string;
-    delivery: {
-        hubs: [];
-        riders: [];
-    };
 }
 
 export type IParcelDoc = HydratedDocument<IParcel>;
