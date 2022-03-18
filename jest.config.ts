2461b780c39b9cf67ea66825f61a575d07631cd8
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-19 01:22:35 +0600

    Add tests for utils

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,17 @@
 import type { Config } from '@jest/types';
 
-// Sync object
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   verbose: true,
-  automock: true,
-  transform: {
-    '^.+\\.(ts)?$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['/node_modules/(?!my-package)(.*)'],
+  automock: false,
+  clearMocks: true,
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageProvider: 'v8',
+  moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['js', 'ts'],
+  roots: ['tests'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$'],
 };
 export default config;
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,13 +1,3 @@
-import { verifyMobileNumber } from '../src/lib';
-
-describe('Testing verifyMobileNumber function', function () {
-  it('should show error if given a string of length less than 11', function () {
-    const result = verifyMobileNumber('0123456789');
-    expect(result).toBe(false);
-  });
-
-  it('should show error if given a string of length bigger than 11', function () {
-    const result = verifyMobileNumber('012345678901');
-    expect(result).toBe(false);
-  });
+describe('Tests for server', () => {
+  it('Should response 404 on GET /', () => {});
 });
