1d7e4ab6b6706c8d6335185c82f79a4979f5c463
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-03-19 02:23:58 +0600

    Add tests for app, utils

---
+++ tests/api/routers/auth.router.test.ts
@@ -0,0 +1,6 @@
+import request from 'supertest';
+import { authRouter } from '../../../src/api/routers';
+
+describe('Tests form authRouter /api/v1/auth', () => {
+  it('should return 401 if GET /auth/login', () => {});
+});
